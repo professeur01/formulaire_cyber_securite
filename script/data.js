eabcb41fc091efd7c562e680e363be21ca0e21b5
ARazak89 <hassane.abdelrazak@gmail.com>
2023-12-14 01:13:41 +0100

    Ajout des fonctionnalités JS

---
+++ script/data.js
@@ -0,0 +1,39 @@
+const datas ={
+    users: [
+        {   id: 1,
+            name:'Groupe1',
+            pass:'Groupe1',
+            level: 0,
+            point: 0
+        },
+        {   id: 2,
+            name:'Groupe2',
+            pass:'Groupe2',
+            level: 0,
+            point: 0
+        },
+    ],
+    reponses: [
+        {
+            id:1,
+            reponse: 'rep1',
+        },
+        {
+            id:2,
+            reponse: 'rep2',
+        },
+        {
+            id:3,
+            reponse: 'rep3',
+        },
+        {
+            id:4,
+            reponse: 'rep4',
+        },
+        {
+            id:5,
+            reponse: 'rep5',
+        },
+    ]
+}
+module.exports (datas);
---
+++ script/index.js
@@ -0,0 +1,32 @@
+
+let connected = false;
+localStorage.setItem('connected', JSON.stringify(connected));
+
+const login = document.getElementById('login');
+const pass = document.getElementById('pass');
+const connection = document.querySelector('.submit');
+
+if (!localStorage.getItem('users')) {
+    localStorage.setItem('users', JSON.stringify(datas.users));
+}
+if (!localStorage.getItem('connected')) {
+    localStorage.setItem('connected', JSON.stringify(connected));
+}
+connection.addEventListener("click", function() {
+    let log = login.value;
+    let password = pass.value;
+ 
+    for (let i = 0; i < datas.users.length; i++) {
+        if (log.trim() === datas.users[i].name && password.trim() === datas.users[i].pass) {
+            localStorage.setItem('utilisateurConnecte', JSON.stringify(datas.users[i]));
+            connected = true;
+            localStorage.setItem('connected', JSON.stringify(connected));
+            alert(`Utilisateur ${datas.users[i].name} connecté avec succès`)
+            break;
+        }
+        else{
+            alert('Login et mot de passe non conforme')
+        }
+    }
+
+  });
